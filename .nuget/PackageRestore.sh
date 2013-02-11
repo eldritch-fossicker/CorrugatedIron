a5bb7ed7699631f149bfb87b51d00fba526b4969
OJ <oj@buffered.io>
2013-02-11 21:48:06 +1000

    Fix package restore on mono

---
+++ .nuget/PackageRestore.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+DIR=`dirname $0`
+export EnableNuGetPackageRestore=true
+
+mkdir -p ${DIR}/../packages
+cd ${DIR}/../packages
+for i in CorrugatedIron CorrugatedIron.Tests CorrugatedIron.Tests.Live
+  do mono --runtime=v4.0 ../.nuget/NuGet.exe install ../$i/packages.config
+done
